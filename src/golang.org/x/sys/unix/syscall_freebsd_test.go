aa3bfbf9c8ab47f8cab716bfdc6b9d23fc4d6ef1
kangyanfei <kangyanfei@baice100.com>
2019-04-19 17:12:14 +0800

    使用新的golang.org/x下面库

--- src/golang.org/x/sys/unix/syscall_freebsd_test.go
+++ src/golang.org/x/sys/unix/syscall_freebsd_test.go
@@ -14,14 +14,13 @@ import (
 	"os/exec"
 	"path"
 	"path/filepath"
-	"runtime"
 	"testing"
 
 	"golang.org/x/sys/unix"
 )
 
 func TestSysctlUint64(t *testing.T) {
-	_, err := unix.SysctlUint64("vm.swap_total")
+	_, err := unix.SysctlUint64("security.mac.labeled")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -158,9 +157,6 @@ func init() {
 }
 
 func TestCapEnter(t *testing.T) {
-	if runtime.GOARCH != "amd64" {
-		t.Skipf("skipping test on %s", runtime.GOARCH)
-	}
 	ExitsCorrectly(t, "cap_enter")
 }
 
@@ -257,9 +253,6 @@ func init() {
 }
 
 func TestOpenat(t *testing.T) {
-	if runtime.GOARCH != "amd64" {
-		t.Skipf("skipping test on %s", runtime.GOARCH)
-	}
 	ExitsCorrectly(t, "openat")
 }
 
@@ -294,19 +287,4 @@ func TestCapRightsSetAndClear(t *testing.T) {
 	if !b {
 		t.Fatalf("Wrong rights set")
 	}
-}
-
-// stringsFromByteSlice converts a sequence of attributes to a []string.
-// On FreeBSD, each entry consists of a single byte containing the length
-// of the attribute name, followed by the attribute name.
-// The name is _not_ NULL-terminated.
-func stringsFromByteSlice(buf []byte) []string {
-	var result []string
-	i := 0
-	for i < len(buf) {
-		next := i + 1 + int(buf[i])
-		result = append(result, string(buf[i+1:next]))
-		i = next
-	}
-	return result
 }
